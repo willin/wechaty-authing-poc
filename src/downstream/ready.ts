b5326b495c1f8c0706aa398987c6264154cf8a69
Willin Wang <willin@willin.org>
2022-01-19 10:33:05 +0800

    feat: add operation codes

--- src/downstream/ready.ts
+++ src/downstream/ready.ts
@@ -35,21 +35,39 @@ export default async function ready(this: Wechaty): Promise<void> {
         ~deletedUsers.findIndex((user) => user.externalId === member.id)
     );
     log.info(name, `members2Delete: ${members2Delete.length}`);
-    await room.say(
+    log.info(
+      name,
       `准备删除成员（注意删除失败的需要放到警告列表中）：\n ${members2Delete
         .map((c) => `${c.name()}(${c.id})`)
         .join('\n')}`
     );
+    const result = await Promise.allSettled(
+      members2Delete?.map((member) => {
+        if (member.friend()) {
+          await this.Contact.delete(member);
+        }
+        return room?.del(member);
+      })
+    );
+    const members2Warning1 = members2Delete?.filter(
+      (_, index) => result[index].status === 'rejected'
+    );
 
-    const members2Warning = memberList.filter(
+    const members2Warning2 = memberList.filter(
       (member) =>
         !~deletedUsers.findIndex((user) => user.externalId === member.id) &&
         !~users.findIndex((user) => user.externalId === member.id)
     );
-    log.info(name, `members2Warning: ${members2Warning.length}`);
+    log.info(
+      name,
+      `members2Warning: ${members2Warning1.length + members2Warning2.length}`
+    );
 
+    // 可以私发管理员通知
     await room.say(
-      `警告需要手动确认状态：\n ${members2Warning
+      `警告需要手动确认状态：\n ${members2Warning1
+        .map((c) => `${c.name()}(${c.id})`)
+        .join('\n')}---\n${members2Warning2
         .map((c) => `${c.name()}(${c.id})`)
         .join('\n')}`
     );
@@ -59,10 +77,14 @@ export default async function ready(this: Wechaty): Promise<void> {
       (friend) => !~memberList.findIndex((member) => member.id === friend.id)
     );
     // 邀请入群
-    await room!.say(
+    log.info(
+      name,
       `邀请已经添加机器人好友的用户：\n ${members2Invite
         .map((c) => `${c.name()}(${c.id})`)
         .join('\n')}`
+    );
+    await Promise.allSettled(
+      members2Invite?.map((member) => room?.add(member))
     );
   } else {
     // 检查未入群的好友
--- src/downstream/webhook.mock.ts
+++ src/downstream/webhook.mock.ts
@@ -14,8 +14,29 @@ export async function listener(this: Wechaty): Promise<void> {
     (member) => ~deletedUsers.findIndex((user) => user.externalId === member.id)
   );
   log.info(name, `members2Delete: ${members2Delete?.length}`);
-  await room!.say(
+
+  log.info(
+    name,
     `准备删除成员（注意删除失败的需要放到警告列表中）：\n ${members2Delete
+      ?.map((c) => `${c.name()}(${c.id})`)
+      .join('\n')}`
+  );
+
+  const result = await Promise.allSettled(
+    members2Delete?.map((member) => {
+      if (member.friend()) {
+        await this.Contact.delete(member);
+      }
+      return room?.del(member);
+    })
+  );
+  const members2Warning = members2Delete?.filter(
+    (_, index) => result[index].status === 'rejected'
+  );
+
+  // 通知可以私发给管理员
+  await room?.say(
+    `请检查以下成员的删除情况：\n ${members2Warning
       ?.map((c) => `${c.name()}(${c.id})`)
       .join('\n')}`
   );
